80ffd7dedee3389417a443bd0a4467f7ea2655a5
KaiqueMunhoz <munhoz13km@gmail.com>
2018-04-24 22:25:44 -0300

    Created the directory 'screens' to separated the screens of app and created the login screen

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import {AppRegistry} from 'react-native';
 import Feed from './components/Feed';
-// import Feed from './screens/Login';
+import Login from './screens/Login';
 
 export default () => {
-  AppRegistry.registerComponent('InstaluraMobile', () => Feed);
+  AppRegistry.registerComponent('InstaluraMobile', () => Login);
 }
