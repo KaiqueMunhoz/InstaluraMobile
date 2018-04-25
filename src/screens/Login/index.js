1cadef3aa76c4894a8878ceb649c20ab1d57300a
KaiqueMunhoz <munhoz13km@gmail.com>
2018-04-24 22:42:20 -0300

    Created the view login with textinput, button and title

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
 import {
     View,
-    StyleSheet
+    StyleSheet,
+    TextInput,
+    Text,
+    Button,
+    Dimensions
 } from 'react-native';
 
 export default class Login extends Component {
+
     render(){
         return(
-            <View>
+            <View style={styles.container}>
+                <Text style={styles.titulo}>
+                    Instalura
+                </Text>
+
+                <View style={styles.form}>
+                    <TextInput 
+                        style={styles.input}
+                        placeholder='Usuario...'
+                        underlineColorAndroid="transparent"/>
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder='Senha...'
+                        underlineColorAndroid="transparent"/>
+                </View>
+
+                <Button title='Login' onPress={() => console.warn('button')} />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    titulo: {
+        fontWeight: 'bold',
+        fontSize: 26
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    form: {
+        width: Dimensions.get('screen').width * 0.8
+    },
+    input: {
+        height: 40,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd',
+        marginBottom: 10
+    }
+})
